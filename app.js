08bd75f8728d3e12ec94781b013ebceeb4fede11
zarpadoen <yaircarbia420@gmail.com>
2022-07-31 20:23:37 -0300

    2da revision

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ class Contenedor {
     constructor(archivo) {
         this.archivo = archivo;
     }
-
     async save(obj) {
         try {
             const lectura = await fs.promises.readFile(this.archivo, "utf-8");
@@ -71,36 +70,43 @@ class Contenedor {
     }
 }
 const contenedor = new Contenedor("./productos.txt");
+const pruebasConsola = async () => {
+    //Agregar productos
+
+    // await contenedor.save({ title: "Gaseosa", price: 300, thumbnail: "gaseosa.png" });
+    // await contenedor.save({ title: "Pizza", price: 500, thumbnail: "pizza.png" });
+    // await contenedor.save({ title: "Pastas", price: 600, thumbnail: "pastas.png" });
+
+    // Luego de agregar los productos , comentar las lineas porque se vuelven agregar.
 
 
-// contenedor.save({
-//     title: "Gaseosa",
-//     price: 300,
-//     thumbnail: "gaseosa.png"
-// });
-// contenedor.save({
-//     title: "Pizza",
-//     price: 500,
-//     thumbnail: "pizza.png"
-// });
-// contenedor.save({
-//     title: "Pastas",
-//     price: 600,
-//     thumbnail: "pastas.png"
-// });
-// contenedor.save({
-//     title: "Agua",
-//     price: 200,
-//     thumbnail: "agua.png"
-// });
+    //Obtener todos los productos
+    let productos = await contenedor.getAll(); //no comentar
+    // console.log("Todos los productos!", productos);
 
 
+    // Obtenemos producto por el id
+    // const producto = await contenedor.getById(3);
+    // console.log("El producto seleccionado segun el  ID es :", producto)
 
-// contenedor.getById(3);
+    //Eliminamos un producto por el id
 
-// contenedor.getAll().then(data => console.log(data))
+    //1ero ver la totalidad de los productos 
+    // console.log("Todos los productos!", productos);
+
+    //Eliminarlo por id
+    // await contenedor.deleteById(1);
 
 
-// contenedor.deleteById(4);
+    //Ver que se elimino el producto
+    // console.log("Todos los productos!", productos);
 
-contenedor.deleteAll();
+
+    //Eliminamos todos los productos
+    // await contenedor.deleteAll();
+
+    //Varificar si se eliminaron
+    // productos = await contenedor.getAll();
+    // console.log('No hay productos ! ', productos);
+}
+pruebasConsola();
