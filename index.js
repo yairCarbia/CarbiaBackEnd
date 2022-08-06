2c9f4996c2173667a1dbe1c2a747e3304034e17f
zarpadoen <yaircarbia420@gmail.com>
2022-08-06 14:32:23 -0300

    Realizado

---
+++ index.js
@@ -0,0 +1,22 @@
+//Implementado server y adquiriendo los metodos de express
+const express = require("express");
+const Contenedor = require("./clase")
+const contenedor = new Contenedor("./productos.txt");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.get("/productos", async (req, res) => {
+    try {
+        const productos = await contenedor.getAll();
+        res.send("Realizado por Carbia Yair, Productos: \n" + JSON.parse(productos));
+    } catch (error) { console.log(error); }
+
+})
+app.get("/productorandom", async (req, res) => {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    const productRandom = await contenedor.getById(randomNumber);
+    res.send(productRandom)
+})
+const server = app.listen(PORT, () => { console.log("Server corriendo en " + PORT); });
+
